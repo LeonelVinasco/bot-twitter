06b2aa3fcb0c52fec28ad8e8f6bf38f22cd6013c
Victor Andriot <vandriot@PORT-LYON-0001.AVNIR.local>
2017-01-02 12:10:16 +0100

    Same commit as before

--- bot.js
+++ bot.js
@@ -10,55 +10,36 @@ var stream = T.stream('user');
 
 stream.on('follow', followed);
 
+// When someone follow you, this funtion is called
 function followed(eventMsg) {
-  //Local command
-  //var cmd = 'processing-java --sketch=`pwd`/random_image --run'
 
-  //Cloud command
-  var cmd = 'random_image/random_image' 
-  exec(cmd, generatedImage);
-  var screenName = eventMsg.source.screen_name ;
+  var cmd = 'random_image/random_image'
+  exec(cmd, generatedImage); // Generation of the image
+  var screenName = eventMsg.source.screen_name ; // Get the name of the personn that followed you
 
+  // When the image is generated, this function is called
   function generatedImage() {
     var b64content = fs.readFileSync('random_image/output.png', { encoding: 'base64' })
     console.log('Image generation finished');
 
-    T.post('media/upload', { media_data: b64content }, uploaded);
+    T.post('media/upload', { media_data: b64content }, uploaded); //Upload of the image to Twitter, the tweet is not yet send.
 
+    // When the image is uploaded, this function is called
     function uploaded(err, data, response) {
       var id = data.media_id_string;
       var tweet = {
         status: '@' + screenName + ' Thank you for following! \n Here is a priceless unique image!',
         media_ids: [id]
       }
-      T.post('statuses/update', tweet, tweeted);
+      T.post('statuses/update', tweet, tweeted); //The tweet is posted.
     }
     function tweeted(err, data, response) {
       if (err) {
         console.log('Something went wrong!');
       } else {
-        console.log('Tweet sent!');
+        console.log('Tweet sent!\n');
       }
     }
   }
   console.log('@' + screenName + ' followed the bot');
 }
-//var text = 'Here is a random number ' + Math.floor(Math.random()*100);
-
-//setInterval(tweetIt, 1000*20);
-
-
-
-// var params = {
-//   q: '#monpremiertweet',
-//   count: 2
-// } ;
-//
-// T.get('search/tweets', params, gotData);
-//
-// function gotData(err, data, response) {
-//   var tweets = data.statuses ;
-//   for (var i = 0; i < tweets.length; i++) {
-//     console.log(tweets[i].text);
-//   }
-// }
